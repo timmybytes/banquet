605d9e5b7ab782cdddb37badb0c53ade28509ad9
Timothy Merritt <timmybytes@gmail.com>
2021-07-24 09:06:57 -0700

    test: Add SCSS, jest config for next/css modules

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 module.exports = {
-  setupFilesAfterEnv: ['./jest.setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  moduleDirectories: ['node_modules', 'src'],
   moduleNameMapper: {
-    '^@components(.*)$': '<rootDir>/components$1',
-    '^@pages(.*)$': '<rootDir>/pages$1',
-    '^@hooks(.*)$': '<rootDir>/hooks$1',
+    '\\.(scss|sass|css)$': 'identity-obj-proxy',
   },
+  testEnvironment: 'jsdom',
 };
--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,18 +1,11 @@
-// test/pages/index.test.js
-
-import HomePage from '@pages/index';
-import React from 'react';
-// Using render and screen from test-utils.js instead of
-// @testing-library/react
+import HomePage from './index';
 import { render, screen } from '../test/test-utils';
 
 describe('HomePage', () => {
   it('should render the heading', () => {
     render(<HomePage />);
 
-    const heading = screen.getByText(
-      /Testing Next.js With Jest and React Testing Library/i
-    );
+    const heading = screen.getByText(/Welcome/i);
 
     // we can only use toBeInTheDocument because it was imported
     // in the jest.setup.js and configured in jest.config.js
