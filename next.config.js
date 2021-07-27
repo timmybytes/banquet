8b14e820156f35c9234b5eda5902ae4c7151a764
Timothy Merritt <timmybytes@gmail.com>
2021-07-27 07:15:17 -0700

    chore: Update config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-  future: { webpack5: false },
-  target: 'serverless',
+  // future: { webpack5: false },
+  // target: 'serverless',
   sassOptions: {
     includePaths: [path.join(__dirname, 'src/styles')],
   },
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,43 +5,66 @@ import Document, {
   Main,
   NextScript,
 } from 'next/document';
-import { createGenerateId, JssProvider, SheetsRegistry } from 'react-jss';
+import React from 'react';
+// import { createGenerateId, JssProvider, SheetsRegistry } from 'react-jss';
 
-export default class _Document extends Document {
+// export default class _Document extends Document {
+//   static async getInitialProps(ctx: DocumentContext) {
+//     const registry = new SheetsRegistry();
+//     const generateId = createGenerateId({ minify: true });
+//     const originalRenderPage = ctx.renderPage;
+
+//     ctx.renderPage = () =>
+//       // @ts-ignore
+//       originalRenderPage({
+//         enhanceApp: App => props =>
+//           (
+//             <JssProvider registry={registry} generateId={generateId}>
+//               <App {...props} />
+//             </JssProvider>
+//           ),
+//       });
+
+//     const initialProps = await Document.getInitialProps(ctx);
+
+//     return {
+//       ...initialProps,
+//       styles: (
+//         <>
+//           {initialProps.styles}
+//           <style id='mantine-ssr-styles'>{registry.toString()}</style>
+//         </>
+//       ),
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head>
+//           <link rel='icon' href='link to favicon' />
+//         </Head>
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     );
+//   }
+// }
+
+// import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
-    const registry = new SheetsRegistry();
-    const generateId = createGenerateId({ minify: true });
-    const originalRenderPage = ctx.renderPage;
-
-    ctx.renderPage = () =>
-      originalRenderPage({
-        enhanceApp: App => props =>
-          (
-            <JssProvider registry={registry} generateId={generateId}>
-              <App {...props} />
-            </JssProvider>
-          ),
-      });
-
     const initialProps = await Document.getInitialProps(ctx);
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <style id='mantine-ssr-styles'>{registry.toString()}</style>
-        </>
-      ),
-    };
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
-        <Head>
-          <link rel='icon' href='link to favicon' />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
@@ -50,3 +73,5 @@ export default class _Document extends Document {
     );
   }
 }
+
+export default MyDocument;
