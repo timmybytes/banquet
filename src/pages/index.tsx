8cdf7e2a313f6b354903c6a98b50717cbc491d8c
Timothy Merritt <67444371+timmybytes@users.noreply.github.com>
2021-08-06 10:06:08 -0700

    Appetizer (#3)
    * chore: Add basic landing page
    
    * chore: Add tech stack info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
-import { Footer } from "@components/Footer";
-import { Card } from "@mantine/core";
-import styles from "@styles/Home.module.scss";
+import {
+  Accordion,
+  AccordionItem,
+  Container,
+  Paper,
+  Text,
+  Title,
+} from "@mantine/core";
 import Head from "next/head";
-import Link from "next/link";
 import React from "react";
 
 export default function Home() {
@@ -13,35 +17,106 @@ export default function Home() {
         <meta name="description" content="A hearty Next.js template" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <h1 className={styles.title}>Welcome to Main Course!</h1>
+      <Container
+        style={{
+          padding: "2rem",
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          shadow="lg"
+          style={{
+            padding: "2rem",
+            height: "80%",
+            width: "80%",
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            justifyContent: "flex-start",
+          }}
+        >
+          <Title
+            align="center"
+            order={1}
+            style={{
+              fontFamily: "Playfair Display",
+              fontStyle: "italic",
+              fontSize: "3rem",
+            }}
+          >
+            Welcome to Main Course!
+          </Title>
 
-        <p className={styles.description}>
-          Visit the{" "}
-          <Link href="/examples">
-            <a>Examples page</a>
-          </Link>
-        </p>
-        <p className={styles.description}>
-          Visit the{" "}
-          <Link href="/mantine">
-            <a>Mantine UI page</a>
-          </Link>
-        </p>
+          <Text style={{ maxWidth: "65ch", padding: "1rem" }}>
+            This is a template for building a static site with Next, Plop,
+            Mantine UI, Typescript, Husky, Prettier, ESLint, and Jest/React
+            Testing Library. Main Course is currently very much a{" "}
+            <strong>work in progress</strong>, so while it will eventually be a
+            robust boilerplate, use at your own risk while it's still being
+            developed!
+          </Text>
 
-        <div className={styles.grid}>
-          <Card shadow="sm" padding={30}>
-            <a href="/">
-              <h2>Get Started</h2>
-              <p>
-                Start building your Next.js + TypeScript + Mantine + CSS Modules
-                + Plop app
-              </p>
-            </a>
-          </Card>
-        </div>
-      </main>
-      <Footer />
+          <Title
+            align="center"
+            order={2}
+            style={{
+              fontFamily: "Playfair Display",
+              fontStyle: "italic",
+              padding: "1rem",
+              fontSize: "2.5rem",
+            }}
+          >
+            Menu
+          </Title>
+          <Accordion>
+            <AccordionItem label="Next.js Features">
+              <a href="https://nextjs.org">Next.js</a> features performance
+              optimization, static site generation, routing, image optimization,
+              and more.
+            </AccordionItem>
+            <AccordionItem label="Automate creation of components, pages, etc.">
+              <a href="https://plopjs.com">Plop.js</a> is a tool for creating
+              code from templates, and is used here to generate the components,
+              pages, and other files that make up your site.
+            </AccordionItem>
+            <AccordionItem label="Mantine UI">
+              <a href="https://mantine.dev">Mantine</a> is a set of React
+              components and custom hooks for building user interfaces.
+            </AccordionItem>
+            <AccordionItem label="Typescript">
+              <a href="https://www.typescriptlang.org">Typescript</a> is a typed
+              superset of JavaScript that compiles to plain JavaScript.
+            </AccordionItem>
+            <AccordionItem label="Husky">
+              <a href="https://husky.github.io">Husky</a> is a Git hooks manager
+              for your project.
+            </AccordionItem>
+            <AccordionItem label="Prettier">
+              <a href="https://prettier.io">Prettier</a> is a tool for
+              formatting your code.
+            </AccordionItem>
+            <AccordionItem label="ESLint">
+              <a href="https://eslint.org">ESLint</a> is a tool for linting
+              JavaScript.
+            </AccordionItem>
+            <AccordionItem label="Jest">
+              <a href="https://jestjs.io">Jest</a> is a testing library for
+              React.
+            </AccordionItem>
+            <AccordionItem label="React Testing Library">
+              <a href="https://testing-library.com/docs/react-testing-library/intro/">
+                React Testing Library
+              </a>{" "}
+              is a testing library for React.
+            </AccordionItem>
+          </Accordion>
+        </Paper>
+      </Container>
     </>
   );
 }
