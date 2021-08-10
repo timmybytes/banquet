bafbd9778ab956a4590fa94658e7b479f9d18b11
Timothy Merritt <67444371+timmybytes@users.noreply.github.com>
2021-08-10 13:22:03 -0700

    appetizer/refactor templates (#5)
    * chore: Improve plopfile, add documentation -- WIP
    
    * docs: Update README & docs

--- plopfile.js
+++ plopfile.js
@@ -1,174 +1,129 @@
-module.exports = (plop) => {
-  plop.setGenerator("Component", {
-    description: "Create a new component with default configuration",
+// Define location of your template files (relative to this file)
+const TEMPLATE_DIR = './templates'
+
+// Define the files that can be generated
+module.exports = plop => {
+  plop.setWelcomeMessage('Choose from the menu options below:')
+  plop.setGenerator('Component', {
+    description: 'Create a new component',
     prompts: [
       {
-        type: "input",
-        name: "name",
-        message: "Enter a name for the component",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-        templateFile: "templates/Component/Component.tsx.hbs",
+        type: 'input',
+        name: 'name',
+        message: 'Enter a name for the component:',
       },
       {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx",
-        templateFile: "templates/Component/Component.test.tsx.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/{{pascalCase name}}.module.scss",
-        templateFile: "templates/Component/Component.module.scss.hbs",
-      },
-      {
-        type: "add",
-        path: "src/components/{{pascalCase name}}/index.ts",
-        templateFile: "templates/Component/index.ts.hbs",
-      },
-    ],
-  });
-  plop.setGenerator("Page", {
-    description: "Create a new page",
-    prompts: [
-      {
-        type: "input",
-        name: "name",
-        message: "Enter a name for the page",
-      },
-    ],
-    actions: [
-      {
-        type: "add",
-        path: "src/pages/{{lowerCase name}}.tsx",
-        templateFile: "templates/Page/Page.tsx.hbs",
-      },
-    ],
-  });
-  // TODO: Pages
-  //   plop.setGenerator('page', {
-  //     description: 'Create a page',
-  //     prompts: [
-  //       {
-  //         type: 'input',
-  //         name: 'name',
-  //         message: 'What is your page name?',
-  //       },
-  //     ],
-  //     actions: [
-  //       {
-  //         type: 'add',
-  //         path: 'src/pages/{{pascalCase name}}/{{pascalCase name}}.js',
-  //         templateFile: 'templates/Page/Page.js.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/pages/{{pascalCase name}}/{{pascalCase name}}.test.js',
-  //         templateFile: 'templates/Page/Page.test.js.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/pages/{{pascalCase name}}/{{pascalCase name}}.module.css',
-  //         templateFile: 'templates/Page/Page.module.css.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/pages/{{pascalCase name}}/index.js',
-  //         templateFile: 'templates/Page/index.js.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/pages/index.js',
-  //         templateFile: 'templates/injectable-index.js.hbs',
-  //         skipIfExists: true,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/pages/index.js',
-  //         pattern: `/* PLOP_INJECT_IMPORT */`,
-  //         template: `import {{pascalCase name}} from './{{pascalCase name}}';`,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/pages/index.js',
-  //         pattern: `/* PLOP_INJECT_EXPORT */`,
-  //         template: `\t{{pascalCase name}},`,
-  //       },
-  //     ],
-  //   });
+        type: 'list',
+        name: 'type',
+        message: 'Select a type:',
 
-  //   plop.setGenerator('service', {
-  //     description: 'Create service',
-  //     prompts: [
-  //       {
-  //         type: 'input',
-  //         name: 'name',
-  //         message: 'What is your service name?',
-  //       },
-  //     ],
-  //     actions: [
-  //       {
-  //         type: 'add',
-  //         path: 'src/services/{{camelCase name}}.js',
-  //         templateFile: 'templates/service.js.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/services/index.js',
-  //         templateFile: 'templates/injectable-index.js.hbs',
-  //         skipIfExists: true,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/services/index.js',
-  //         pattern: `/* PLOP_INJECT_IMPORT */`,
-  //         template: `import {{camelCase name}} from './{{camelCase name}}';`,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/services/index.js',
-  //         pattern: `/* PLOP_INJECT_EXPORT */`,
-  //         template: `\t{{camelCase name}},`,
-  //       },
-  //     ],
-  //   });
+        // Create single or multiple files from a template
+        choices: [
+          {
+            name: 'TSX Component with SCSS module, test, and index file',
+            value: 'full',
+          },
+          {
+            name: 'TSX Component only',
+            value: 'basic',
+          },
+        ],
+      },
+    ],
 
-  //   plop.setGenerator('hook', {
-  //     description: 'Create a custom react hook',
-  //     prompts: [
-  //       {
-  //         type: 'input',
-  //         name: 'name',
-  //         message: 'What is your hook name?',
-  //       },
-  //     ],
-  //     actions: [
-  //       {
-  //         type: 'add',
-  //         path: 'src/hooks/{{camelCase name}}.js',
-  //         templateFile: 'templates/hook.js.hbs',
-  //       },
-  //       {
-  //         type: 'add',
-  //         path: 'src/hooks/index.js',
-  //         templateFile: 'templates/injectable-index.js.hbs',
-  //         skipIfExists: true,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/hooks/index.js',
-  //         pattern: `/* PLOP_INJECT_IMPORT */`,
-  //         template: `import {{camelCase name}} from './{{camelCase name}}';`,
-  //       },
-  //       {
-  //         type: 'append',
-  //         path: 'src/hooks/index.js',
-  //         pattern: `/* PLOP_INJECT_EXPORT */`,
-  //         template: `\t{{camelCase name}},`,
-  //       },
-  //     ],
-  //   });
-};
+    actions: function (data) {
+      // Fallback name for the component if one isn't provided - randomized to avoid duplicates
+      if (!data.name) {
+        data.name = `Component__${Math.random().toString(36).substr(2, 5)}`
+      }
+
+      var actions = []
+
+      if (data.type === 'full') {
+        actions.push(
+          {
+            type: 'add',
+            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+            templateFile: `${TEMPLATE_DIR}/Component/Component.tsx.hbs`,
+          },
+          {
+            type: 'add',
+            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.test.tsx',
+            templateFile: `${TEMPLATE_DIR}/Component/Component.test.tsx.hbs`,
+          },
+          {
+            type: 'add',
+            path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.module.scss',
+            templateFile: `${TEMPLATE_DIR}/Component/Component.module.scss.hbs`,
+          },
+          {
+            type: 'add',
+            path: 'src/components/{{pascalCase name}}/index.ts',
+            templateFile: `${TEMPLATE_DIR}/Component/index.ts.hbs`,
+          }
+        )
+      } else {
+        actions.push({
+          type: 'add',
+          path: 'src/components/{{pascalCase name}}/{{pascalCase name}}.tsx',
+          templateFile: `${TEMPLATE_DIR}/Component/Component.tsx.hbs`,
+        })
+      }
+
+      return actions
+    },
+  }),
+    plop.setGenerator('Page', {
+      description: 'Create a new page',
+      prompts: [
+        {
+          type: 'input',
+          name: 'name',
+          message: 'Enter a name for the page:',
+        },
+      ],
+      actions: function (data) {
+        // Fallback name for the component if one isn't provided - randomized to avoid duplicates
+        if (!data.name) {
+          data.name = `Component__${Math.random().toString(36).substr(2, 5)}`
+        }
+
+        var actions = []
+
+        actions.push({
+          type: 'add',
+          path: 'src/pages/{{lowerCase name}}.tsx',
+          templateFile: `${TEMPLATE_DIR}/Page/Page.tsx.hbs`,
+        })
+
+        return actions
+      },
+    })
+
+  // Create further generators in the same way, ex:
+  // plop.setGenerator('Layout', {
+  //   description: 'Create a new layout',
+  //   prompts: [
+  //     {
+  //       type: 'input',
+  //       name: 'name',
+  //       message: 'Enter a name for the layout',
+  //     },
+  //   ],
+  //   actions: function (data) {
+  //     if (!data.name) {
+  //       data.name = 'Layout'
+  //     }
+
+  //     var actions = []
+
+  //     actions.push({
+  //       type: 'add',
+  //       path: 'src/layouts/{{lowerCase name}}.tsx',
+  //       templateFile: 'templates/Layout/Layout.tsx.hbs',
+  //     })
+
+  //     return actions
+  //   }
+  // })
+}
