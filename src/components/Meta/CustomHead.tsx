7f9f41a264293f613d2f079d3984021bfd6048c2
Timothy Merritt <timmybytes@gmail.com>
2021-08-10 19:22:01 -0700

    chore: Updates `<meta>` info & repo card

--- src/components/Meta/CustomHead.tsx
+++ src/components/Meta/CustomHead.tsx
@@ -1,17 +1,17 @@
 interface MetaHeadProps {
-  title?: string;
+  title?: string
   og?: [
     {
-      property: string;
-      content: string;
+      property: string
+      content: string
     }
-  ];
+  ]
 }
 
 const HEAD_DATA = {
-  title: "Main Course",
-  description: "",
-};
+  title: 'Main Course',
+  description: 'A hearty Next.js boilerplate',
+}
 
 /* Example Open Graph meta tags
 
@@ -35,76 +35,72 @@ const HEAD_DATA = {
 // Add app-wide meta tags for _document.tsx head
 export const MetaHead = ({ title, og }: MetaHeadProps) => (
   <>
-    <meta name="title" content="timmybytes" key="meta-title" />
+    <meta name='title' content='timmybytes' key='meta-title' />
     <meta
-      name="description"
-      content="timmybytes is a portfolio and blog for Timothy Merritt | Developer, Designer, Writer, and Musician"
-      key="description"
+      name='description'
+      content='Main Course is a boilerplate for Next.js projects with build-in support for TypeScript, SCSS, Jest, Handlebars, Chakra UI, and more.'
+      key='description'
     />
     <link
-      rel="apple-touch-icon"
-      sizes="180x180"
-      href="/favicons/apple-touch-icon.png"
-      key="apple-touch-icon"
+      rel='apple-touch-icon'
+      sizes='180x180'
+      href='/favicons/apple-touch-icon.png'
+      key='apple-touch-icon'
     />
     <link
-      rel="icon"
-      type="image/png"
-      sizes="32x32"
-      href="/favicons/favicon-32x32.png"
-      key="favicon-32x32"
+      rel='icon'
+      type='image/png'
+      sizes='32x32'
+      href='/favicons/favicon-32x32.png'
+      key='favicon-32x32'
     />
     <link
-      rel="icon"
-      type="image/png"
-      sizes="16x16"
-      href="/favicons/favicon-16x16.png"
-      key="favicon-16x16"
+      rel='icon'
+      type='image/png'
+      sizes='16x16'
+      href='/favicons/favicon-16x16.png'
+      key='favicon-16x16'
     />
     <link
-      rel="mask-icon"
-      href="/safari-pinned-tab.svg"
-      color="#5bbad5"
-      key="safari-pinned-tab"
+      rel='mask-icon'
+      href='/safari-pinned-tab.svg'
+      color='#5bbad5'
+      key='safari-pinned-tab'
     />
-    <meta name="msapplication-TileColor" content="#da532c" key="ms-tile" />
-    <meta name="theme-color" content="#ff90ad" key="theme-color" />
-    <meta property="og:locale" content="en_US" key="og-locale" />
-    <meta property="og:type" content="website" key="og-type" />
-    <meta property="og:title" content="timmybytes" key="og-title" />
+    <meta name='msapplication-TileColor' content='#da532c' key='ms-tile' />
+    <meta name='theme-color' content='#ff90ad' key='theme-color' />
+    <meta property='og:locale' content='en_US' key='og-locale' />
+    <meta property='og:type' content='website' key='og-type' />
+    <meta property='og:title' content='timmybytes' key='og-title' />
     <meta
-      property="og:description"
-      content="timmybytes is a portfolio and blog for Timothy Merritt | Developer, Designer, Writer, and Musician"
-      key="og-description"
-    />
-    <meta property="og:url" content="https://timmybytes.com" key="og-url" />
-    <meta
-      property="og:image"
-      content="https://raw.githubusercontent.com/timmybytes/timmybytes-blog/main/public/images/timmybytes-Plain-Business-OG-Card.png"
-      key="og-img"
-    />
-    <meta property="og:image:type" content="image/png" key="og-img-type" />
-    <meta
-      property="og:image:alt"
-      content="The timmybytes logo"
-      key="og-img-alt"
-    />
-    <meta property="og:image:width" content="1200" key="og-img-width" />
-    <meta property="og:image:height" content="647" key="og-img-height" />
-    <meta
-      property="twitter:card"
-      content="summary_large_image"
-      key="twitter-card"
+      property='og:description'
+      content='Main Course is a boilerplate for Next.js projects with build-in support for TypeScript, SCSS, Jest, Handlebars, Chakra UI, and more.'
+      key='og-description'
     />
     <meta
-      property="twitter:image"
-      content="https://timmybytes.com/images/timmybytes-Plain-Business-OG-Card.png"
-      key="twitter-img"
+      property='og:url'
+      content='https://github.com/timmybytes/main-course'
+      key='og-url'
+    />
+    <meta property='og:image' content='/main-course-card.png' key='og-img' />
+    <meta property='og:image:type' content='image/png' key='og-img-type' />
+    <meta property='og:image:alt' content='Main Course logo' key='og-img-alt' />
+    <meta property='og:image:width' content='1200' key='og-img-width' />
+    <meta property='og:image:height' content='647' key='og-img-height' />
+    <meta
+      property='twitter:card'
+      content='summary_large_image'
+      key='twitter-card'
     />
     <meta
-      property="twitter:creator"
-      content="@timmybytes"
-      key="twitter-creator"
+      property='twitter:image'
+      content='/main-course-card.png'
+      key='twitter-img'
+    />
+    <meta
+      property='twitter:creator'
+      content='@timmybytes'
+      key='twitter-creator'
     />
   </>
-);
+)
