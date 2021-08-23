746a72a51e2911f31e2cc494fa58303efc70df23
Timothy Merritt <timmybytes@gmail.com>
2021-08-23 13:00:41 -0700

    chore: Add feature list to section --WIP

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,17 +33,43 @@ export default function Home(): React.ReactNode {
           <Box p={{ base: '0', md: '2rem' }}>
             <Heading my='1rem'>Features</Heading>
             <Text as='p' m='1rem 0' maxW='65ch'>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
-              incidunt minima laudantium quo et possimus natus non? Mollitia,
-              suscipit in nobis dignissimos dolorem laborum illum iusto quia
-              ducimus, ab deleniti?
+              Main Course is an opinionated boilerplate for creating{' '}
+              <a href='https://nextjs.org'>Next.js</a> projects. There are{' '}
+              <em>many</em> great starter templates out there for settings up a
+              web development project, but there always seems to be a few things
+              useful defaults missing from most of them. Main Course aims to
+              solve this by front-loading more tools and configurations as
+              defaults with less tinkering required.
             </Text>
-            <Text>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
-              laboriosam earum nihil commodi cupiditate, quasi, esse assumenda
-              hic ullam totam dignissimos debitis doloribus veritatis, quo fuga
-              veniam quia dolor. Quo?
-            </Text>
+            <Text>It's configured with the following out of the box:</Text>
+            <Box
+              as='ul'
+              d='flex'
+              flexDirection='column'
+              listStyleType='none'
+              p='1.5rem'>
+              <Box as='li' p='.5rem'>
+                Modern React DX: <a href='https://nextjs.org'>Next.js</a> +{' '}
+                <a href=''>Typescript</a>
+              </Box>
+              <Box as='li' p='.5rem'>
+                Flexible Styling: Support for <a href=''>CSS Modules</a>,{' '}
+                <a href=''>SASS</a>, and/or CSS-in-JS
+              </Box>
+              <Box as='li' p='.5rem'>
+                Testing defaults: <a href=''>Jest</a> +{' '}
+                <a href=''>React Testing Library</a>
+              </Box>
+              <Box as='li' p='.5rem'>
+                Robust UI: <a href=''>Chakra UI</a> + <a href=''>React Icons</a>
+              </Box>
+              <Box as='li' p='.5rem'>
+                Automated Component & page generation: <a href=''>Plop.js</a>
+              </Box>
+              <Box as='li' p='.5rem'>
+                CI/CD: <a href=''>Husky</a>
+              </Box>
+            </Box>
           </Box>
         </Box>
       </Section>
