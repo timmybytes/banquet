9f1df8296e373035bc40bbebce12242ed8502642
Timothy Merritt <timmybytes@gmail.com>
2021-08-23 12:47:44 -0700

    chore: Add basic feature sections to homepage

--- src/components/Layout/Section/Section.tsx
+++ src/components/Layout/Section/Section.tsx
@@ -2,11 +2,15 @@ import { Box } from '@chakra-ui/react'
 
 type SectionProps = {
   children?: React.ReactNode
+  [x: string]: any
 }
 
-export const Section = ({ children }: SectionProps): React.ReactElement => {
+export const Section = ({
+  children,
+  ...rest
+}: SectionProps): React.ReactElement => {
   return (
-    <Box as='section' my={5} px='32px'>
+    <Box as='section' my={5} px='32px' {...rest}>
       {children}
     </Box>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text } from '@chakra-ui/react'
+import { Box, Button, Heading, Image, Text } from '@chakra-ui/react'
 import { Hero } from '@components/Hero'
 import { Section } from '@components/Layout/Section'
 import { SITE_DATA } from '@components/Meta'
@@ -15,51 +15,141 @@ export default function Home(): React.ReactNode {
         ctaLabel={SITE_DATA.hero.ctaLabel}
         smallText={SITE_DATA.hero.smallText}
       />
-      <Section>
-        <Heading>
-          About{' '}
-          <Text
-            as='span'
-            fontStyle='italic'
-            fontFamily='Playfair Display'
-            fontWeight='900'>
-            Main Course
-          </Text>
-        </Heading>
-        <Box
-          as='ul'
-          d='flex'
-          flexDir='column'
-          justifyContent='flex-start'
-          alignItems='flex-start'
-          p='1.5rem'>
-          <Box as='li'>
-            <Text>
-              <strong>Feature</strong>
+      <Section
+        display='flex'
+        flexWrap='wrap'
+        justifyContent='space-between'
+        alignItems='center'
+        flexDirection={{ base: 'column', md: 'row' }}>
+        <Box maxW={{ base: '100%', md: '50%' }} p={{ base: '0' }}>
+          <Image
+            src='/images/visual-stories-micheile-eF4421ZnFWU-unsplash@1000.jpg'
+            size='100%'
+            rounded='1rem'
+            shadow='2xl'
+          />
+        </Box>
+        <Box maxW={{ base: '100%', md: '50%' }} p={{ base: '0' }}>
+          <Box p={{ base: '0', md: '2rem' }}>
+            <Heading my='1rem'>Features</Heading>
+            <Text as='p' m='1rem 0' maxW='65ch'>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+              incidunt minima laudantium quo et possimus natus non? Mollitia,
+              suscipit in nobis dignissimos dolorem laborum illum iusto quia
+              ducimus, ab deleniti?
             </Text>
-          </Box>
-          <Box as='li'>
             <Text>
-              <strong>Feature</strong>
-            </Text>
-          </Box>
-          <Box as='li'>
-            <Text>
-              <strong>Feature</strong>
-            </Text>
-          </Box>
-          <Box as='li'>
-            <Text>
-              <strong>Feature</strong>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Atque
+              laboriosam earum nihil commodi cupiditate, quasi, esse assumenda
+              hic ullam totam dignissimos debitis doloribus veritatis, quo fuga
+              veniam quia dolor. Quo?
             </Text>
           </Box>
         </Box>
-        <Text as='p' m='1rem 0' maxW='65ch'>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi incidunt
-          minima laudantium quo et possimus natus non? Mollitia, suscipit in
-          nobis dignissimos dolorem laborum illum iusto quia ducimus, ab
-          deleniti?
-        </Text>
+      </Section>
+      <Section
+        p='2rem'
+        d='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Configured for modern React development with Typescript + Next.js
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Support for CSS Modules + SASS
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Jest + React Testing Library preconfigured
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Chakra UI
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Plop.js Templates + Code Generator
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+        <Box>
+          <Text as='h3' my='1rem' fontSize='2rem' maxW='30ch' fontWeight='900'>
+            Continuous Integration with Husky
+          </Text>
+          <Text as='p' m='1rem 0' maxW='65ch'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
+            incidunt minima laudantium quo et possimus natus non? Mollitia,
+            suscipit in nobis dignissimos dolorem laborum illum iusto quia
+            ducimus, ab deleniti?
+          </Text>
+        </Box>
+      </Section>
+      <Section
+        p='2rem'
+        d='flex'
+        flexDirection='column'
+        justifyContent='center'
+        alignItems='center'>
+        <Box
+          d='flex'
+          flexDirection='column'
+          alignItems='center'
+          justifyContent='center'
+          maxW='45ch'
+          textAlign='center'
+          sx={{ gap: '1rem' }}>
+          <Text
+            fontSize='2rem'
+            fontFamily='Playfair Display'
+            fontStyle='italic'
+            fontWeight='900'>
+            Starting building with
+            <br />
+            Main Course today
+          </Text>
+          <Button
+            color='brand.dark'
+            bg='brand.gold'
+            _hover={{ color: 'brand.gold', bg: 'brand.dark' }}>
+            View on GitHub
+          </Button>
+        </Box>
       </Section>
     </>
   )
